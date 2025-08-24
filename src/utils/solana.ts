bc40c8f2add05874580f2af19aa315ceda5bfa06
Jovian Shilton Dsouza <dsouzajovian123@gmail.com>
2025-08-24 20:15:03 +0530

    feat: update Solana utility functions to load wallet from environment and utilize bs58 for key decoding; add bs58 dependency in package.json and package-lock.json

--- src/utils/solana.ts
+++ src/utils/solana.ts
@@ -1,13 +1,22 @@
-import { Connection, PublicKey, LAMPORTS_PER_SOL } from '@solana/web3.js';
+import { bs58 } from '@coral-xyz/anchor/dist/cjs/utils/bytes';
+import { Connection, LAMPORTS_PER_SOL, Keypair } from '@solana/web3.js';
 
 const connection = new Connection('https://api.devnet.solana.com');
-const wallet = new PublicKey('9CtkxgXqNF3yvGr4u9jdVByyZknBH4SoqPgNpRbX2sjP');
+export async function loadWalletFromEnv() {
+    const keyBase58 = process.env['WALLET_PRIVATE_KEY'];
+    if (!keyBase58) throw new Error("Private key env var not found.");
+    const secretKey = bs58.decode(keyBase58);
+    const wallet = Keypair.fromSecretKey(secretKey);
+    console.log("Wallet public key:", wallet.publicKey.toBase58());
+    return wallet;
+}
 
 export async function requestAirdrop() {
     console.log('Requesting airdrop...');
+    const wallet = await loadWalletFromEnv();
     try {
-        const signature = await connection.requestAirdrop(wallet, 1 * LAMPORTS_PER_SOL);
-        console.log(`Airdropped 1 SOL to ${wallet.toBase58()}`);
+        const signature = await connection.requestAirdrop(wallet.publicKey, 1 * LAMPORTS_PER_SOL);
+        console.log(`Airdropped 1 SOL to ${wallet.publicKey.toBase58()}`);
         return signature;
     } catch (error) {
         console.error('Error requesting airdrop');
